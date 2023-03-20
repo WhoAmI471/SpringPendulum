cd5facb7a5bbfe3955ebba8b4ba48e1bbab5c2d9
WhoAmI471 <artglad05@mail.ru>
2023-03-20 20:59:24 +0500

    new table

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const res = document.querySelector('#answer');
 const pr = document.querySelector('#pr');
 const mas = document.querySelector('#mas');
 const kRan = document.querySelector('#k');
-const check = document.querySelector('#check');
+// const check = document.querySelector('#check');
 const root = document.querySelector(':root');
 
 
@@ -11,7 +11,7 @@ document.querySelector('#masa').oninput = masCount;
 document.querySelector('#rigidity').oninput = kCount;
 document.querySelector('#btn').onclick = update;
 
-check.onclick = checedSystem;
+// check.onclick = checedSystem;
 
 
 let m0 = 0;
@@ -48,12 +48,14 @@ function masCount() {
     m = (this.value/1000).toFixed(2);
     mas.innerHTML = `${m} Кг.`;
     block.style.setProperty("--m", `${m*10}px`);
+    update();
 };
 
 function kCount() {
     k = this.value;
     console.log(k);
     kRan.innerHTML = `${k} H/м`;
+    update();
 };
 
 function update() {
@@ -64,7 +66,9 @@ function update() {
 
     //высота пружины и амплитуда сжатия
     root.style.setProperty("--h", `${h}px`);
+    
     //root.style.setProperty("--z");если понадобится сделать увеличение толщены пружины
+
     a = x * 100 //перевод в сантиметры
     root.style.setProperty("--x0", `${h+a}px`);
     root.style.setProperty("--x1", `${h-a}px`);
@@ -73,6 +77,16 @@ function update() {
 function Stop(){
     pr.style.setProperty("--T", `${0}s`);
 }
+
+function getkInTable(id){
+    k = parseFloat(document.querySelector(`#${id}`).textContent);
+
+    kRan.innerHTML = `${k} H/м`;
+    update();
+}
+
+
+// часть полностью на переделку
 
 function eventX(value) {
    x = parseFloat(value)/10;
